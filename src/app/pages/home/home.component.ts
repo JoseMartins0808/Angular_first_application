4dc10bc4dcddda4b1b964e1b6f691ff6679e37d8
JoseMartins0808 <ottenazuose@hotmail.com>
2024-03-06 20:11:14 -0300

    feat: user cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
-
+  homeText = "Página principal";
+  userA = { name: "Alex", email: "alex@mail.com", job: "Instrutor" };
+  userB = { name: "José", email: "jose@mail.com", job: "Programador" };
 }
