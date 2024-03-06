5cff4ca8b93b3285757218ebc121b87a2d1946b0
JoseMartins0808 <ottenazuose@hotmail.com>
2024-03-06 19:46:01 -0300

    feat: nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "contato", component: ContactComponent }
+    { path: "contato", component: ContactComponent },
+    { path: "sobre", component: AboutPageComponent }
 ];
