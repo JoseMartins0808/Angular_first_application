5d844208885e61042ba142cbf139ec6e090753a7
JoseMartins0808 <ottenazuose@hotmail.com>
2024-03-07 17:27:04 -0300

    feat: loop renderization at home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { UserCardComponent } from '../../components/user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserCardComponent],
+  imports: [CommonModule, UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
   homeText = "Página principal";
-  userA = { name: "Alex", email: "alex@mail.com", job: "Instrutor" };
-  userB = { name: "José", email: "jose@mail.com", job: "Programador" };
+  userList = [{ name: "Alex", email: "alex@mail.com", job: "Instrutor" },
+  { name: "José", email: "jose@mail.com", job: "Programador" },
+  { name: "Maria", email: "maria@mail.com", job: "Programadora Front End" }]
 }
